82ba88d44163d4b2f230d116b586b44a010fe9c5
Steve LeValley <slevalley@gmail.com>
2022-05-18 08:47:16 -0700

    Added some basic html - Ended 28:51

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Content from "./layout/Content";
+import Header from "./layout/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layout/Sidebar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
+
+function Sidebar() {
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li>
+          <span>Inbox</span>
+          <span>
+            <FaInbox />
+          </span>
+        </li>
+        <li>
+          <span>Today</span>
+          <span>
+            <FaRegCalendar />
+          </span>
+        </li>
+        <li>
+          <span>Next 7 Days</span>
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+        </li>
+      </ul>
+      <div className="sidebar__middle">
+        <span>
+          <FaChevronDown />
+        </span>
+        <h2>Projects</h2>
+      </div>
+      <ul className="sidebar__projects">Projects will be here!</ul>
+      Add Project Component Here!
+    </div>
+  );
+}
+
+export default Sidebar;
